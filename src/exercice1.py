c3250f32430d72eda803f117c21ee3689315a55b
Fabien Lamarque <fabien.lamarque+git@gmail.com>
2015-12-03 01:01:14 +0100

    #01st Exercice + #01.2 Exercice + tests

--- src/exercice1.py
+++ src/exercice1.py
@@ -10,3 +10,15 @@ def calculatestairs(parameter):
             count -= 1
 
     return count
+
+
+def basemententrance(parameter):
+    count = 0
+    for index, char in enumerate(parameter):
+        if char == "(":
+            count += 1
+        else:
+            count -= 1
+            if count < 0:
+                return index + 1
+    return index
--- test/Exercice1Test.py
+++ test/Exercice1Test.py
@@ -2,6 +2,7 @@ __author__ = 'fabienlamarque'
 
 import unittest
 from src.exercice1 import calculatestairs
+from src.exercice1 import basemententrance
 from utils.FileReader import readfile
 
 
@@ -21,8 +22,18 @@ class Exercice1Test(unittest.TestCase):
     def test_two_stairs_up_one_stair_down(self):
         self.assertEquals(calculatestairs("(()"), 1)
 
-    def test_file_reading(self):
+    def test_calculate_stairs_with_file(self):
         self.assertEquals(calculatestairs(readfile("resources/1x1.txt")), 280)
+
+    def test_direct_basement_entrance(self):
+        self.assertEquals(basemententrance(")"), 1)
+
+    def test_basement_third_step(self):
+        self.assertEquals(basemententrance("())"), 3)
+
+    def test_basement_entrance_with_file(self):
+        self.assertEquals(basemententrance(readfile("resources/1x1.txt")), 1797)
+
 
 
 if __name__ == '__main__':
